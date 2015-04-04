bb30c6e375300766e13756028a4e3803581a2690
jahooo666 <okraskaj@iem.pw.edu.pl>
2015-04-04 18:07:47 +0200

    pierwsze podejscie do wordArray -bedzie potrzebna tablica dynamiczna.

---
+++ wordArray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+word_t *wordArray;
+
+int createWord(char* newWord);
+word_t* findWord(word_t *wordArray, char *soughtWord);
+void addOccurence(wordt_t word);
+
+	
+
