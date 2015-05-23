005f6bfac4f1e938a5bc54688fec43f36c34b601
jahooo666 <okraskaj@iem.pw.edu.pl>
2015-05-23 18:35:56 +0200

    Powrot po przerwie, zmieniony schemat generowania tekstu- wierze ze to z tego moglo wynikac narusszenie pamieci

--- gener.c
+++ gener.c
@@ -6,11 +6,15 @@
 #include "gener.h"
 
 void gener(node * tree, int rng, int ls){
-	int i,j;
-	for(i=0;i<ls;i++){
-		for(j=0;j<ls;j++){
+// ls -liczba slow do wygenerowania
+	int i,j;	
+	
+	//drukowanie wszystkich wyrazow pierwszego ngramu
+	for(j=0;j<rng;j++)
 			printf("%s \n",tree->wArray[j]);
-		}
-		tree = (tree->nextArray)[0].nextEl; 
+	//drukowanie pojedynczego(ostatniego) slowa kolejnego ngramu i przesuwanie wskaznika na nastepny element		
+	while(tree){		
+		printf("%s \n",tree->wArray[rng-1]);
+		tree = (tree->nextArray)[0].nextEl;	 
 	}
 }
